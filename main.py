26a7bf02afb752b3dec54816a554fbfc92927c60
erin-a <erineadler@gmail.com>
2019-03-28 18:30:27 -0500

    thursday's updates

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
-import random
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route("/")
-def index():
-    # choose a movie by invoking our new function
-    movie = get_random_movie()
+header = """
+    <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Flicklist</title>
+                </head>
+                <body>
+                    <h1>Welcome to Flicklist</h1>
+    """
 
-    # build the response string
-    content = "<h1>Movie of the Day</h1>"
-    content += "<ul>"
-    content += "<li>" + movie + "</li>"
-    content += "</ul>"
+footer="""
+            </body>
+        </html>
+    """
 
-    # TODO: pick another random movie, and display it under
-    # the heading "<h1>Tommorrow's Movie</h1>"
-    tom_movie = get_random_movie()
-
-    tom_content = "<h1>Tomorrow's Movie</h1>"
-    tom_content += "<ul>"
-    tom_content += "<li>" + tom_movie + "</li>"
-    tom_content += "</ul>"
-
-    return content + tom_content
-
-def get_random_movie():
+@app.route("/add")
+def add_movie():
+    new_movie = request.args.get("movie")
     
-    # TODO: make a list with at least 5 movie titles
-    movie_list = ["movie1", "movie2", "movie3", "movie4", "movie5"]
-    # TODO: randomly choose one of the movies, and return it
-    selected_movie = random.choice(movie_list)
-    #return "The Big Lebowski"
-    return selected_movie
+    content = header + "<p>" + new_movie + " has been added to your watchlist.</p>" + footer
 
-    #tom_selected_movie = random.choice(movie_list)
-    #return tom_selected_movie
-    #if tom_selected_movie != selected_movie:
-    #    return tom_selected_movie
-    #else:
-    #    return tom_selected_movie + 1
+    return content
 
+@app.route("/")
+# create form that allows user to tell us what movies they want to watch
+# add it to list
+
+def index():
+    content = header + """
+                <form action="/add" method="get">
+                    <label for="movie">I want to add
+                    <input type="text" name="movie" id="movie" />
+                    to my watchlist.</label>
+                    <input type="submit" value="Add it" />
+                </form?
+    """ + footer
+
+    return content
 
 app.run()
